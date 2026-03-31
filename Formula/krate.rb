35c5053baf04b1d5942166207fa6653336e63394
todd <todd@miniFu.local>
2026-03-31 09:36:25 -0400

    fix: remove post_install auto-start, improve caveats

--- Formula/krate.rb
+++ Formula/krate.rb
@@ -4,20 +4,21 @@ class Krate < Formula
   version "0.10.0"
   license "Commercial"
 
+  url "https://github.com/kannister-app/krate-vm-engine/archive/refs/tags/v0.10.0.tar.gz"
+  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+
   depends_on :macos
   depends_on arch: :arm64
 
-  # Bottle will be populated by krate-build pipeline.
-  # Until then, this formula builds from source (requires Rust).
-  # bottle do
-  #   root_url "https://ghcr.io/v2/kannister-app/krate/krate-bottle"
-  #   sha256 cellar: :any_skip_relocation, arm64_monterey: "TBD"
-  # end
+  bottle do
+    root_url "file:///tmp"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ad199c36a3bb4895b06f3343a98253c066142adbb6e0ca950c1983c73ff1a065"
+  end
 
   def install
-    odie "krate requires Apple Silicon (M1 or later)" unless Hardware::CPU.arm?
-    bin.install "krate" if File.exist? "krate"
-    bin.install "anvild" if File.exist? "anvild"
+    bin.install "krate"
+    bin.install "anvild"
+    (share/"krate").install "io.kannister.anvild.plist" if File.exist? "io.kannister.anvild.plist"
   end
 
   service do
@@ -27,17 +28,10 @@ class Krate < Formula
     error_log_path var/"log/krate/anvild-error.log"
   end
 
-  def post_install
-    system "brew", "services", "start", "krate"
-  end
-
   def caveats
     <<~EOS
-      anvild starts automatically on login.
-
-      Manage manually:
+      To start anvild now and auto-start on login:
         brew services start krate
-        brew services stop krate
 
       Note: `brew upgrade krate` restarts anvild and stops running workloads.
 
